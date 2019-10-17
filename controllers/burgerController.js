8f54fe653dfe27eca97ae01706441b5bc89a701a
MattRoger <mrogerphotography@gmail.com>
2019-10-16 21:23:53 -0600

    converted over to burgers 404 error

---
+++ controllers/burgerController.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burgers.js");
+
+router.get("/", function (req, res) {
+
+  burger.all(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+router.post("/api/burgers", function (req, res) {
+  burger.create([
+    "name", "eaten"
+  ], [
+    req.body.name, req.body.eaten
+  ], function (result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burger.update({
+    eaten: req.body.eaten
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
+
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+module.exports = router;
---
+++ public/assets/script/burgerApp.js
@@ -0,0 +1,39 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
+$(function() {
+    $(".change-eat").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEat = $(this).data("neweat");  
+      var newEatenState = {
+        eaten: newEat
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("changed eat to", newEat);
+          // location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+        var newBurger = {
+       name: $("#ca").val().trim(),
+       eaten: $("[name=eaten]:checked").val().trim()
+      };
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // location.reload();
+        }
+      );
+    });
+  });
+  
